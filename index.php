15418265c5fe704e04786dfc2a0576a1b94524a3
CShadowMan <126810672+CShadowMan@users.noreply.github.com>
2023-03-02 21:51:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,11 +9,64 @@
 </head>
 <body>
     <h1>Best sellers</h1>
+    <div class="row resources ">
      <div class="container" id="resource-slider">
      <button class='arrow prev'><img src="images/prev.svg" alt="prev"></button>
     <button class='arrow next'><img src="images/next.svg" alt="next"></button>
     </div>
-    <div class="slider"></div>
+         <div class="resource-slider-frame">
+
+         <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+ <div class="resource">
+            <img src="images/p/1.jpg" alt="1jpg">
+            <footer class="hmt small">Cena</footer>
+        </div>
+        </div>
+        </div>
+         <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+        <div class="resource">
+           <img src="images/p/2.jpg" alt="2jpg">
+            <footer class="hmt small">cena</footer>
+        </div>
+        </div>
+        </div>
+         <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+    <div class="resource">
+           <img src="images/p/3.jpg" alt="3jpg">
+            <footer class="hmt small">cena</footer>
+        </div>
+        </div>
+        </div>
+         <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+    <div class="resource">
+           <img src="images/p/4.jpg" alt="4jpg">
+            <footer class="hmt small">cena</footer>
+        </div>
+        </div>
+        </div>
+         <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+    <div class="resource">
+           <img src="images/p/5.jpg" alt="5jpg">
+            <footer class="hmt small">cena</footer>
+        </div>
+        </div>
+        </div>
+         <div class="resource-slider-item">
+        <div class="resource-slider-inset">
+    <div class="resource">
+           <img src="images/p/6.jpg" alt="6jpg">
+            <footer class="hmt small">cena</footer>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    
 </body>
 </html>
 <?php
